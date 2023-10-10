2b9a6ed778e15400046e9c534cde3b1bf9bb3f3b
blinchikkk <blinchikpm@gmail.com>
2023-10-10 13:58:36 +0700

    Update main.py
    + reboot
    + boot installer

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import os
 import subprocess
 import sys
 
+
 def check_file_exists(file_path):
     return os.path.exists(file_path)
 
+
 def run_fastboot_command():
     try:
-        process = subprocess.Popen(['fastboot', 'devices'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+        process = subprocess.Popen(
+            ['fastboot', 'devices'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
         while True:
             output = process.stdout.readline()
             if not output and process.poll() is not None:
@@ -16,11 +19,13 @@ def run_fastboot_command():
             sys.stdout.flush()
     except subprocess.CalledProcessError as e:
         print(f"Ошибка выполнения команды fastboot: {e.output}")
+
 
 def install_recovery(method):
     try:
         option = 'recovery_ab' if method == 1 else 'recovery'
-        process = subprocess.Popen(['fastboot', 'flash', option, 'recovery.img'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+        process = subprocess.Popen(['fastboot', 'flash', option, 'recovery.img'],
+                                   stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
         while True:
             output = process.stdout.readline()
             if not output and process.poll() is not None:
@@ -29,6 +34,56 @@ def install_recovery(method):
             sys.stdout.flush()
     except subprocess.CalledProcessError as e:
         print(f"Ошибка выполнения команды fastboot: {e.output}")
+
+
+def install_boot(method):
+    try:
+        option = 'boot_ab' if method == 1 else 'boot'
+        process = subprocess.Popen(['fastboot', 'flash', option, 'boot.img'],
+                                   stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+        while True:
+            output = process.stdout.readline()
+            if not output and process.poll() is not None:
+                break
+            sys.stdout.write(output)
+            sys.stdout.flush()
+    except subprocess.CalledProcessError as e:
+        print(f"Ошибка выполнения команды fastboot: {e.output}")
+
+
+def reboot(method, type):
+    def return_method(method, type):
+        option = 'adb' if method == 1 else 'fastboot'
+        reboot_type = None
+        if method == 1:
+            if type == 2:
+                reboot_type = 'fastboot'
+            elif type == 3:
+                reboot_type = 'recovery'
+            else:
+                reboot_type = ''
+        else:
+            if type == 2:
+                reboot_type = 'recovery'
+            else:
+                reboot_type = ''
+
+        return [option, reboot_type]
+
+    try:
+        option = return_method(method, type)[0]
+        reboot_location = return_method(method, type)[1]
+
+        process = subprocess.Popen([option, reboot_location])
+        while True:
+            output = process.stdout.readline()
+            if not output and process.poll() is not None:
+                break
+            sys.stdout.write(output)
+            sys.stdout.flush()
+    except subprocess.CalledProcessError as e:
+        print(f"Ошибка выполнения команды fastboot: {e.output}")
+
 
 def main(active=True):
     while active:
@@ -43,7 +98,8 @@ def main(active=True):
         if choice == '1':
             run_fastboot_command()
         elif choice == '2':
-            recovery_img_path = os.path.join(os.path.dirname(__file__), 'images', 'recovery.img')
+            recovery_img_path = os.path.join(
+                os.path.dirname(__file__), 'images', 'recovery.img')
             if check_file_exists(recovery_img_path):
                 print('[1] fastboot flash recovery_ab (Рекомендуем).\n'
                       '[2] fastboot flash recovery (Прошивка в активный слот).\n')
@@ -54,10 +110,53 @@ def main(active=True):
                     print('Ошибка! Метод не найден!')
             else:
                 print('Ошибка! Образ Recovery не найден. Пожалуйста, убедитесь, что файл называется recovery.img и находится в папке "images"')
+
+        elif choice == '3':
+            boot_img_path = os.path.join(
+                os.path.dirname(__file__), 'images', 'boot.img')
+            if boot_img_path:
+
+                print('[1] fastboot flash boot_ab (Рекомендуем).\n'
+                      '[2] fastboot flash boot (Прошивка в активный слот).\n')
+
+                method = input('Ваш выбор: ')
+
+                if method in ['1', '2']:
+                    install_boot(method)
+                else:
+                    print('Ошибка! Метод не найден!')
+            else:
+                print(
+                    'Ошибка! Образ Ядра не найден. Пожалуйста, убедитесь, что файл называется boot.img и находится в папке "images"')
+
+        elif choice == '4':
+            print('[1] Перезагрузка с помощью ADB.\n'
+                  '[2] Перезагрузка с помощью FastBoot. (Если усиройство находится в режиме FastBoot)')
+
+            method = input('Ваш выбор: ')
+
+            if method in ['1', '2']:
+                if method == '1':
+                    print('[1] Перезагрузка.\n'
+                          '[2] Перезагрузка в FastBoot.\n'
+                          '[3] Перезагрузка в Recovery.\n')
+                    del method
+                    method = input('Ваш выбор: ')
+                    if method in ['1', '2', '3']:
+                        reboot(1, method)
+
+                elif method == '2':
+                    print('[1] Перезагрузка в систему.\n'
+                          '[2] Перезагрузка в Recovery.\n')
+                    method = input('Ваш выбор: ')
+                    if method in ['1', '2']:
+                       reboot(2, method) 
+
         elif choice == '5':
             active = False
         else:
             print('Ошибка! Неверный выбор!')
+
 
 if __name__ == "__main__":
     main()
