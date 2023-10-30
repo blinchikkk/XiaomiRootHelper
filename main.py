9084b5d04d55c6627390cc5241912af1d051eef1
blinchikkk <blinchikpm@gmail.com>
2023-10-31 01:48:35 +1000

    mtk client

--- main.py
+++ main.py
@@ -1,21 +1,46 @@
 import os
 from functions import fb_devices, make_backup, install_recovery, \
-    install_boot, reboot
-from messages import * 
+    install_boot, reboot, unlock_bootloader
+from messages import *
+from colorama import init, Fore, Back, Style
+
+init()
+
+def return_text():
+    platform_name = 'Windows' if os.name == 'nt' else 'Linux'
+    version = "v.1.2.0"
+
+    text = f"""{Fore.CYAN}
+$$\   $$\ $$\                                   $$\ $$$$$$$\  $$\   $$\ 
+$$ |  $$ |\__|                                  \__|$$  __$$\ $$ |  $$ |
+\$$\ $$  |$$\  $$$$$$\   $$$$$$\  $$$$$$\$$$$\  $$\ $$ |  $$ |$$ |  $$ |
+ \$$$$  / $$ | \____$$\ $$  __$$\ $$  _$$  _$$\ $$ |$$$$$$$  |$$$$$$$$ |
+ $$  $$<  $$ | $$$$$$$ |$$ /  $$ |$$ / $$ / $$ |$$ |$$  __$$< $$  __$$ |
+$$  /\$$\ $$ |$$  __$$ |$$ |  $$ |$$ | $$ | $$ |$$ |$$ |  $$ |$$ |  $$ |
+$$ /  $$ |$$ |\$$$$$$$ |\$$$$$$  |$$ | $$ | $$ |$$ |$$ |  $$ |$$ |  $$ |
+\__|  \__|\__| \_______| \______/ \__| \__| \__|\__|\__|  \__|\__|  \__|{Style.RESET_ALL}                                                
+                    platform: {Fore.RED}{platform_name}{Style.RESET_ALL}
+                    version: {Fore.GREEN}{version}{Style.RESET_ALL}
+    """
+    print(text)
+
 def main():
+    return_text()
     base_path = os.path.dirname(__file__)
     
     while True:
         print('[1] Посмотреть устройства в FastBoot.\n'
               '[2] Установка Recovery.\n'
               '[3] Установка Ядра.\n'
-              '[4] Перезагрузка.\n'
-              '[5] Выход.\n')
+              '[4] Инструменты.\n'
+              '[5] Перезагрузка.\n'
+              '[10] Выход.\n')
         
         choice = input('Ваш выбор >> ')
-        
+#----------------------------------------------------
         if choice == '1':
             fb_devices()
+#----------------------------------------------------
         elif choice == '2':
             print(fastboot_choise_2_first)
             
@@ -26,7 +51,7 @@ def main():
                 install_recovery(int(choice))
             else:
                 print(method_error)
-            
+#----------------------------------------------------
         elif choice == '3':
             print(fastboot_choise_3_first)
             choice = input('Ваш выбор >> ')
@@ -36,15 +61,25 @@ def main():
                 install_boot(int(choice))
             else:
                 print(method_error)
+#----------------------------------------------------
         elif choice == '4':
+            print(instruments_menu)
+            choice = input('Ваш выбор >> ')
+
+            if choice == 1:
+                unlock_bootloader()
+            elif choice == 2:
+                print('In developing...')
+#----------------------------------------------------
+        elif choice == '5':
             print(reboot_message_first)
             choice = input('Ваш выбор >> ')
             
             if choice in ['1','2','3','4','5']:
                 reboot(choice)
-        elif choice == '5':
+#----------------------------------------------------
+        elif choice == '10':
             break
-        
         else:
             print(method_error)
             
--- messages.py
+++ messages.py
@@ -1,3 +1,7 @@
+from colorama import init, Fore, Back, Style
+
+init()
+
 fastboot_choise_2_first = '''\n[1] fastboot flash recovery_ab (Рекомендуем).
 [2] fastboot flash recovery (Прошивка в активный слот).\n'''
 
@@ -12,3 +16,6 @@ reboot_message_first = '''\n[1] Перезагрузка (ADB)
 [5] Перезагрука в FastBoot. (ADB)\n'''
 
 method_error = 'Ошибка! Метод не найден!'
+
+instruments_menu = f'''\n[1] Разблокировка загрузчика ({Fore.RED}MediaTek Only!{Style.RESET_ALL}).
+[2] Очистка разделов.'''
